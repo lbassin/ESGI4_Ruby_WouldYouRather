07d20f18a958116ee616c19a7188145013df2c2c
Laurent Bassin <laurent@bassin.info>
2018-07-03 20:14:03 +0200

    #16 Hash password

--- Gemfile
+++ Gemfile
@@ -65,3 +65,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'simple_form'
 gem 'jquery-rails'
 gem 'bootstrap-sass'
+gem 'bcrypt'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_215901) do
+ActiveRecord::Schema.define(version: 2018_07_03_180949) do
 
   create_table "questions", force: :cascade do |t|
     t.string "label"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "enabled", default: false
+    t.boolean "enabled"
   end
 
   create_table "responses", force: :cascade do |t|
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2018_07_02_215901) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_hash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
