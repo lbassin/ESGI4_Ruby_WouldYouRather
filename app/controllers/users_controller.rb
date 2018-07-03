d73b7987b436a9f05bb9d85a9693073b62541a04
Laurent Bassin <laurent@bassin.info>
2018-07-03 20:26:06 +0200

    #16 Fix re-hash password if not changed when editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   def update
     data = user_params
     if data[:password].empty?
-      data[:password] = @user.password
+      data.delete(:password)
     end
 
     if @user.update(data)
